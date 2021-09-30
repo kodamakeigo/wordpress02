342215a32622480a92c798f0a9f69668d30d2d03
児玉恵吾 <k.kei5.0205@gmail.com>
2021-09-30 21:02:20 +0900

    2021.09.30 commit.

--- footer.php
+++ footer.php
@@ -1,5 +1,5 @@
 <div id="pagetop" class="pagetop">
-	<img src="<?php echo esc_url(get_template_directory_uri() ); ?>/img/arrow.png" width="40" alt="スクロール">
+	<img src="<?php echo esc_url( get_template_directory_uri() ); ?>/img/arrow.png" width="40" height="40" alt="ページトップ">
 </div>
 <footer>
 	<small>&copy; MdN Corporation.</small>
